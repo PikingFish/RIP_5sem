399e8e7ef1c4a8a79b16470ae520768cc1ba8d31
zkh-dot <kernzahar@gmail.com>
2023-01-16 12:19:43 +0300

    статус: в чате

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../features/auth/authSlice";
 import userReducer from "../features/auth/userSlice";
 import listReducer from "../features/list/listSlice";
+import chatReducer from "../features/chat/chatSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     list: listReducer,
+    chat: chatReducer,
   }
 });
